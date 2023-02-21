5ed49c8389b274d148a0de47b159c7e550b761f4
Professional <new99kpicem@gmail.com>
2023-02-21 22:18:50 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        //task3();
+        //task4();
+        //task5();
+        //task6();
+        //task7();
+        //task8();
+        //task9();
+        //task10();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int bank = 0;
+        int deposit = 15000;
+        int counter = 0;
+        while (bank <= 2459000) {
+            bank = bank + deposit;
+            counter = bank / deposit;
+            System.out.println("Месяц " + counter + ", сумма накоплений равна " + bank + " рублей");
+        }
+    }
+    public static void task2() {
+
     }
 }
