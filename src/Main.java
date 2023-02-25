39801ec57aa7bd0b3834ec646a1711b518ac0d63
Professional <new99kpicem@gmail.com>
2023-02-25 20:01:33 +0300

    3

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         task1();
         task2();
         task3();
-        //task4();
+        task4();
         //task5();
         //task6();
         //task7();
@@ -34,12 +34,29 @@ public class Main {
         for (; i >= 1; i = i - 1) {
             System.out.print(i + " ");
         }
+        System.out.println();
     }
     public static void task3() {
-        int population = 12000000;
-        int birthRate = 17 * population / 1000;
-        int deathRate = 8 * population / 1000;
-
-
+        System.out.println("Задача 3");
+        double population = 12_000_000;
+        double birthRate = 17 / population * 1000;
+        double deathRate = 8 / population * 1000;
+        for (int i=1; i <= 10; i=i+1) {
+            population = population + birthRate - deathRate;
+            System.out.println("Год " + i + ", численность населения составляет " + population);
+        }
+    }
+    public static void task4() {
+        int salary = 65535;
+        int total = 0;
+        int i = 0;
+        for (; total < 1_000_000; i++) {
+            total = total + total/100;
+            total = total + salary;
+            if (i % 5 == 0) {
+                System.out.println("Месяц " + i + " Итого " + total);
+            }
+        }
+        System.out.println(total);
     }
 }
